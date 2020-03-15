13422c79592d15dd810d8ab33cecc67ff2913f8e
Johan Vergeer <johan.vergeer@ordina.nl>
2020-03-15 12:26:55 +0100

    Create empty module with same name as project

---
+++ cookiecutter_poetry.py
@@ -0,0 +1,4 @@
+"""Added as the main module for Poetry.
+
+Without this module Poetry won't be able to install,
+because the Cookiecutter template dir has to be named {{cookiecutter.project.slug}}"""
